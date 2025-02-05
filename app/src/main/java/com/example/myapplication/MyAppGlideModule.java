4862588a4b433ea68b80f36b71327f6758973d7e
daeng <jyjo522i@gmail.com>
2025-02-06 05:10:40 +0900

    Fix: 매직넘버 제거

--- app/src/main/java/com/example/myapplication/MyAppGlideModule.java
+++ app/src/main/java/com/example/myapplication/MyAppGlideModule.java
@@ -13,15 +13,20 @@ import com.bumptech.glide.module.AppGlideModule;
 
 @GlideModule
 public class MyAppGlideModule extends AppGlideModule {
+    private static final int MEMORY_CACHE_SCREEN_COUNT = 2;
+    private static final long DISK_CACHE_SIZE_MB = 250;
+    private static final long MEGABYTE_TO_BYTE = 1024 * 1024;
+
     @Override
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
         // 메모리 캐시 크기 설정
         MemorySizeCalculator calculator = new MemorySizeCalculator.Builder(context)
-                .setMemoryCacheScreens(2)
+                .setMemoryCacheScreens(MEMORY_CACHE_SCREEN_COUNT)
                 .build();
         builder.setMemoryCache(new LruResourceCache(calculator.getMemoryCacheSize()));
 
         // 디스크 캐시 크기 설정
-        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, 250 * 1024 * 1024)); // 250MB
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context,
+                DISK_CACHE_SIZE_MB * MEGABYTE_TO_BYTE)); // 250MB
     }
 }
