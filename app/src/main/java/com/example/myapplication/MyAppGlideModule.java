67f5b7bb98aa1eb38414f9e42a66b93898926897
daeng <jyjo522i@gmail.com>
2025-02-06 05:09:31 +0900

    Add: MyAppGlideModule

---
+++ app/src/main/java/com/example/myapplication/MyAppGlideModule.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        // 메모리 캐시 크기 설정
+        MemorySizeCalculator calculator = new MemorySizeCalculator.Builder(context)
+                .setMemoryCacheScreens(2)
+                .build();
+        builder.setMemoryCache(new LruResourceCache(calculator.getMemoryCacheSize()));
+
+        // 디스크 캐시 크기 설정
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, 250 * 1024 * 1024)); // 250MB
+    }
+}
